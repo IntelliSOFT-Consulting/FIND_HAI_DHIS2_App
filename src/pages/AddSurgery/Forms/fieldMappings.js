03286039791dd5ad4bb07bbeeb8b280b20e06dab
Michael Ndula <michaelndula@gmail.com>
2023-09-27 10:16:52 +0300

    Separated the fieldMappings to a different component for reusability

---
+++ src/pages/AddSurgery/Forms/fieldMappings.js
@@ -0,0 +1,29 @@
+export const fieldMappings = {
+    "Surgical Site Case Report Form Patient ID": {
+      label: "Patient ID",
+    },
+    "Surgical Site Case Report Form Secondary ID": {
+      label: "Secondary ID",
+    },
+    "Surgical Site Case Report Form Gender": {
+      label: "Gender",
+    },
+    "Surgical Site Case Report Form Date of Birth": {
+      label: "Date of Birth",
+    },
+    "Surgical Site Case Report Form Date of Admission": {
+      label: "Date of Admission",
+    },
+    "Surgical Site Case Report Form Date of Surgery": {
+      label: "Date of Surgery",
+    },
+    "Surgical Site Case Report Form Surgical Procedure": {
+      label: "Surgical Procedure",
+    },
+    "Surgical Site Case Report Form Scheduling": {
+      label: "Scheduling",
+    },
+    "Surgical Site Case Report Form Surgery Location": {
+      label: "Surgery Location",
+    },
+  };
